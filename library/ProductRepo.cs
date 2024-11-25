388f409d14484b30fe3d5cce341f35017c8ca587
Khim070 <109895928+Khim070@users.noreply.github.com>
2024-11-25 17:27:39 +0700

    Initial commit

---
+++ library/ProductRepo.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace productlib
+{
+    public class ProductRepo
+    {
+        private readonly DbContext _context;
+
+        public ProductRepo(DbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> CreateAsync(Product entity)
+        {
+            _context.Set<Product>().Add(entity.Clone());
+            return _context.SaveChangesAsync();
+        }
+
+        public Task<int> CreateAsync(List<Product> products)
+        {
+            _context.Set<Product>().AddRange(products);
+            return _context.SaveChangesAsync();
+        }
+
+        public IQueryable<Product> GetQueryable() => _context.Set<Product>().AsQueryable();
+
+        public Task<int> UpdateAsync(Product entity)
+        {
+            var found = GetQueryable().FirstOrDefault(x => x.Id == entity.Id);
+            if (found != null)
+            {
+                found.Copy(entity);
+                return _context.SaveChangesAsync();
+            }
+            return Task.FromResult(0);
+        }
+
+        public Task<int> DeleteAsync(string id)
+        {
+            var found = _context.Set<Product>().FirstOrDefault(x => x.Id == id);
+            if(found == null)
+            {
+                return Task.FromResult(0);
+            }
+
+            var result = _context.Set<Product>().Remove(found);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
